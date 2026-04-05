7ae771cb7b5ce72d1dd982385bc0e56d41f6410c
Seek Plush <192367367+SeekPlush-linux@users.noreply.github.com>
2026-04-06 00:40:53 +0500

    feat: add working wordle game logic, improve error handling, fix alternate terminal butter switch

--- wordle-CLI.py
+++ wordle-CLI.py
@@ -3,10 +3,14 @@ import sys
 import tty
 import termios
 import traceback
+import re
+import requests
+from datetime import datetime
 from rich.console import Console
 from rich.panel import Panel
 
-VERSION = "v0.0.0-alpha"
+VERSION = "v0.1.0-alpha"
+guesses = 6
 
 _print = print
 con = Console(highlight=False)
@@ -57,16 +61,58 @@ def print_colored_chars(chars: str, colors: list[str]) -> None:
 
     print(string, justify="center")
 
+status = 0
+
 try:
-    _print("\033[?1047h\033[?25l\r\033[1000A", end="")
+    _print("\033[?1049h\r\033[1000A", end="")
     print_panel(f"[bold][green]Wordle CLI[/] [bright_white]{VERSION}[/][/]")
-    input()
+
+    print("\n[bright_yellow]Fetching today's wordle...[/]")
+
+    now = datetime.now()
+    r = requests.get(f"https://www.nytimes.com/svc/wordle/v2/{now.year}-{now.month:02}-{now.day:02}.json")
+    r.raise_for_status()
+    word = r.json()['solution'].upper()
+
+    print("[bright_green]Successfully fetched![/]\n")
+
+    while True:
+        user_inp = input(f"({guesses})> ").upper()
+
+        if len(user_inp) != 5:
+            print("Isn't 5 letters long!")
+            continue
+
+        if not bool(re.fullmatch("[A-Z]+", user_inp)):
+            print("Must contain only letters!")
+            continue
+
+        colors = []
+        for i, c in enumerate(user_inp):
+            if c == word[i]:
+                colors.append("green")
+            elif c in word:
+                colors.append("yellow")
+            else:
+                colors.append("black")
+
+        print_colored_chars(user_inp, colors)
+
+        if all([x == "green" for x in colors]):
+            print("[bright_green]You guessed today's wordle! Congrats![/]")
+            break
+
+        guesses -= 1
+
+        if guesses == 0:
+            print(f"[bright_red]You failed to guess today's wordle :c[/]\nToday's wordle is: [bold]{word}[/]")
+            break
 
 except Exception:
-    _print("\033[?1047l\033[?25h", end="")
+    status = 1
     traceback.print_exc()
-    exit(1)
 
-else:
-    _print("\033[?1047l\033[?25h", end="")
-    exit(0)
+finally:
+    input("\nPress Enter to exit.\n")
+    _print("\033[?1049l", end="")
+    sys.exit(status)
