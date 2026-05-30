45a005cb01c01cc16503626308732aa3531e6116
Seek Plush <192367367+SeekPlush-linux@users.noreply.github.com>
2026-05-30 18:20:58 +0500

    add todo

--- wordle-CLI.py
+++ wordle-CLI.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from rich.console import Console
 from rich.panel import Panel
 
+# TODO: improve "press any key to exit" message
 # TODO: use different box characters as some (if not, many) fonts don't support some of the chars
 # TODO: add mouse support to be able to use the on-screen keyboard
 
